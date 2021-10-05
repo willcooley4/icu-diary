cc94ff9e8b785050e6ef972b73d3ec916c7d6454
William Emory Cooley <wcooley@umich.edu>
2021-10-05 19:34:16 -0400

    Database initialization works

--- diary/schema.sql
+++ diary/schema.sql
@@ -1,8 +1,6 @@
-PRAGMA foreign_keys = ON;
-
-DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS diary_entry;
-DROP TABLE IF EXISTS diary;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS diary_entries;
+DROP TABLE IF EXISTS diaries;
 DROP TABLE IF EXISTS contributors;
 
 CREATE TABLE users (
@@ -12,29 +10,29 @@ CREATE TABLE users (
     profile_pic TEXT NOT NULL
 );
 
+CREATE TABLE diaries (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    patient TEXT NOT NULL
+);
+
 CREATE TABLE diary_entries (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     contents TEXT NOT NULL,
     media TEXT,
     author TEXT NOT NULL,
-    diary_id INTEGER NOT NULL
-    FOREIGN KEY (author) REFERENCES users (username),
-    FOREIGN KEY (diary_id) REFERENCES diaries (id)
-);
-
-CREATE TABLE diaries (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    patient TEXT NOT NULL
+    diary_id INTEGER NOT NULL,
+    FOREIGN KEY(author) REFERENCES users(username),
+    FOREIGN KEY(diary_id) REFERENCES diaries(id)
 );
 
 CREATE TABLE contributors (
     contributor TEXT NOT NULL,
-    diary_id TEXT NOT NULL,
+    diary_id INTEGER NOT NULL,
     primary_contributor BOOLEAN NOT NULL,
-    FOREIGN KEY (contributor) REFERENCES users (username),
-    FOREIGN KEY (diary_id) REFERENCES diaries (id),
+    FOREIGN KEY(contributor) REFERENCES users(username),
+    FOREIGN KEY(diary_id) REFERENCES diaries(id),
     PRIMARY KEY (contributor, diary_id)
 );
