3e65267e34c2fe90aab269624dad0daa5ba49ec8
Dakota Paasman <dpaasman@umich.edu>
2021-10-05 19:47:39 -0400

    updated init

--- diary/__init__.py
+++ diary/__init__.py
@@ -8,8 +8,10 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'diary.sqlite'),
+        DATABASE="postgres://postgres:password@127.0.0.1:5432/icu_diary"
+        # DATABASE=os.path.join(app.instance_path, 'diary.'),
     )
+    
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -28,5 +30,50 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
+
+    # home page for all users
+    # shows linked pages the user has access to
+    @app.route('/')
+    def home():
+        #todo
+        return ''
+
+    # diary entry page for doctor and contributors
+    # allows for standard, templated, or media submissions
+    @app.route('/entry_page')
+    def entry_page():
+        #todo
+        return ''
+
+    # diary viewing page for patient
+    # scrollable page for diary entries, allows for pdf download
+    @app.route('/diary_vieiwing')
+    def diary_viewing():
+        #todo
+        return ''
+
+    # diary sharing page for contributors
+    # allows user to share diary with outside user via email, text, social media
+    @app.route('/diary_sharing')
+    def diary_sharing():
+        #todo
+        return ''
+    
+    # signup approval page for primary contributor 
+    # allows user to see who has requested access to diary and accept/decline join requests
+    @app.route('/signup_approval')
+    def signup_approval():
+        #todo
+        return ''
+
+    # diary creation page that allows doctor to create new patient diary
+    @app.route('/create_diary')
+    def create_diary():
+        #todo
+        return ''
+    
 
     return app
